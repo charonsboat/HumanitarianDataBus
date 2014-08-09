2f55456b53903718988e47953cae17cda6edf93e
Jon <binaryjanitory@outlooK.com>
2014-08-09 11:49:07 -0500

    Added Events table

---
+++ HumanitarianEventstore/HumanitarianEventstore/Tables/Events.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Events]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [EventXml] XML NULL, 
+    [PropertyXml] XML NULL, 
+    [LastModifiedDateTime] DATETIME NULL, 
+    [LastModifiedUser] VARCHAR(64) NULL
+)
