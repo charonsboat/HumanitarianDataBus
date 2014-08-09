be34cae6891da0a75334f65fcbb98a1fe99dcb70
William A. Homer <whomer@codeokla.com>
2014-08-09 14:14:52 -0500

    Created WebAPI, removed individual Client and Pub projects

--- HumanitarianEventstore/Humanitarian.WebAPI/Properties/AssemblyInfo.cs
+++ HumanitarianEventstore/Humanitarian.WebAPI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HumanitarianPublicationServices")]
+[assembly: AssemblyTitle("Humanitarian.WebAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumanitarianPublicationServices")]
+[assembly: AssemblyProduct("Humanitarian.WebAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb9cacfb-06c4-4ef4-bba6-ef334a3d292f")]
+[assembly: Guid("cbc393f2-3951-47a1-84db-59c60d0f4a20")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
