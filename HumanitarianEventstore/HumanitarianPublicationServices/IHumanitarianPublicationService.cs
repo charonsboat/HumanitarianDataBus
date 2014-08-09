de0fa5026f6632484c24ec3b1a236fb40f5943bb
Jon <binaryjanitory@outlook.com>
2014-08-09 14:35:36 -0500

    Stuff

---
+++ HumanitarianEventstore/HumanitarianPublicationServices/IHumanitarianPublicationService.cs
@@ -0,0 +1,21 @@
+﻿using Humanitarian.DataContracts;
+using Humanitarian.DataContracts.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HumanitarianPublicationServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IHumanitarianPublicationService
+    {
+        [OperationContract]
+        HumanitarianEvent GetEvents(GetEventRequest request);
+    }
+
+}
