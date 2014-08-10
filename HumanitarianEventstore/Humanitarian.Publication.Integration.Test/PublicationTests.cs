4595ae6a6b1c5a5bdff60bad6e68d3422099f916
Jon <binaryjanitory@outlook.com>
2014-08-10 12:56:28 -0500

    Added test data

--- HumanitarianEventstore/Humanitarian.Publication.Integration.Test/PublicationTests.cs
+++ HumanitarianEventstore/Humanitarian.Publication.Integration.Test/PublicationTests.cs
@@ -37,6 +37,50 @@ namespace Humanitarian.Publication.Integration.Test
             }            
         }
 
+        [DeploymentItem(@"Schemas\HumanitarianEventSchema.xsd")]
+        [DeploymentItem(@"Schemas\HumanitarianEventSchema.xml")]
+        [DeploymentItem(@"Schemas\HumanitarianEventProperty.xsd")]
+        [DeploymentItem(@"Schemas\HumanitarianEventProperty.xml")]
+        [DeploymentItem(@"TestData\HumanitarianEventTestData1.xml")]
+        [DeploymentItem(@"TestData\HumanitarianEventTestData2.xml")]
+        [TestMethod]
+        public void AddNewTestEvents()
+        {
+
+            var ex = CreateEventXml("HumanitarianEventTestData1.xml");
+            var px = CreatePropertyXml();
+
+
+            using (var client = new HumanitarianPublicationServices.HumanitarianPublicationServicesClient())
+            {
+                client.AddHumanitarianEvent(new HumanitarianPublicationServices.AddHumanitarianEventRequest()
+                {
+                    EventToAdd = new HumanitarianPublicationServices.HumanitarianEvent()
+                    {
+                        EventEnvelopeXml = ex.ToString(),
+                        EventPropertyXml = px.ToString()
+                    }
+                });
+
+
+                var ex2 = CreateEventXml("HumanitarianEventTestData2.xml");
+                var px2 = CreatePropertyXml();
+
+
+                using (var client2 = new HumanitarianPublicationServices.HumanitarianPublicationServicesClient())
+                {
+                    client.AddHumanitarianEvent(new HumanitarianPublicationServices.AddHumanitarianEventRequest()
+                    {
+                        EventToAdd = new HumanitarianPublicationServices.HumanitarianEvent()
+                        {
+                            EventEnvelopeXml = ex2.ToString(),
+                            EventPropertyXml = px2.ToString()
+                        }
+                    });
+                }
+            }
+        }
+
         private static StringBuilder CreatePropertyXml()
         {
             //string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"HumanitarianEventSchema.xsd");
@@ -67,6 +111,35 @@ namespace Humanitarian.Publication.Integration.Test
             return sb;
         }
 
+        private static StringBuilder CreatePropertyXml(string fileName)
+        {
+            //string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"HumanitarianEventSchema.xsd");
+            XmlSchemaSet schemas = new XmlSchemaSet();
+
+            //Load schema from deployment item
+            schemas.Add("http://schemas.humanitariantoolbox.com/HumanitarianPropertySchema/1/0/0/0/", "HumanitarianEventProperty.xsd");
+
+            //load XML from deployment item
+            var doc = XDocument.Load(fileName, LoadOptions.SetBaseUri);
+            doc.Root.SetDefaultXmlNamespace("http://schemas.humanitariantoolbox.com/HumanitarianPropertySchema/1/0/0/0/");
+
+            // Valid according to the schema
+            doc.Validate(schemas, null, true);
+
+            //Create XML with schema defintion
+            var sb = new StringBuilder();
+            var xws = new XmlWriterSettings();
+
+            xws.OmitXmlDeclaration = true;
+            xws.Indent = true;
+            xws.WriteEndDocumentOnClose = true;
+
+            using (var xw = XmlWriter.Create(sb, xws))
+            {
+                doc.WriteTo(xw);
+            }
+            return sb;
+        }
         private static StringBuilder CreateEventXml()
         {
             //string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"HumanitarianEventSchema.xsd");
@@ -77,6 +150,36 @@ namespace Humanitarian.Publication.Integration.Test
 
             //load XML from deployment item
             var doc = XDocument.Load("HumanitarianEventSchema.xml", LoadOptions.SetBaseUri);
+            doc.Root.SetDefaultXmlNamespace("http://schemas.humanitariantoolbox.com/HumanitarianEventSchema/1/0/0/0/");
+
+            // Valid according to the schema
+            doc.Validate(schemas, null, true);
+
+            //Create XML with schema defintion
+            var sb = new StringBuilder();
+            var xws = new XmlWriterSettings();
+
+            xws.OmitXmlDeclaration = true;
+            xws.Indent = true;
+            xws.WriteEndDocumentOnClose = true;
+
+            using (var xw = XmlWriter.Create(sb, xws))
+            {
+                doc.WriteTo(xw);
+            }
+            return sb;
+        }
+
+          private static StringBuilder CreateEventXml(string fileName)
+        {
+            //string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"HumanitarianEventSchema.xsd");
+            XmlSchemaSet schemas = new XmlSchemaSet();
+
+            //Load schema from deployment item
+            schemas.Add("http://schemas.humanitariantoolbox.com/HumanitarianEventSchema/1/0/0/0/", "HumanitarianEventSchema.xsd");
+
+            //load XML from deployment item
+            var doc = XDocument.Load(fileName, LoadOptions.SetBaseUri);
             doc.Root.SetDefaultXmlNamespace("http://schemas.humanitariantoolbox.com/HumanitarianEventSchema/1/0/0/0/");
 
             // Valid according to the schema
